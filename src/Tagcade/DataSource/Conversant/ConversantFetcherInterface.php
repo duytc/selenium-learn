f84b68182bb664b1f7eb2b416158a8245f396e60
Hieu Tran <dtag.dev1@gmail.com>
2016-06-28 16:43:46 +0700

    create Conversant

---
+++ src/Tagcade/DataSource/Conversant/ConversantFetcherInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tagcade\DataSource\Conversant;
+
+use Tagcade\DataSource\PartnerFetcherInterface;
+
+interface ConversantFetcherInterface extends PartnerFetcherInterface {
+
+} 
---
+++ src/Tagcade/DataSource/Conversant/Widget/AbstractWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Tagcade\DataSource\Conversant\Widget;
+
+use Tagcade\DataSource\PulsePoint\Widget\AbstractWidget as BaseAbstractWidget;
+
+class AbstractWidget extends BaseAbstractWidget  {
+
+} 
