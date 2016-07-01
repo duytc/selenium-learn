a1e8db5c61c83895bd8b0a6b5be51557b2cb0378
Hieu Tran <dtag.dev1@gmail.com>
2016-07-01 15:54:14 +0700

    report Ads4Games for everyday

--- src/Tagcade/Bundle/AppBundle/Command/Ads4Games/GetDataCommand.php
+++ src/Tagcade/Bundle/AppBundle/Command/Ads4Games/GetDataCommand.php
@@ -1,10 +1,11 @@
 <?php
 
-
 namespace Tagcade\Bundle\AppBundle\Command\Ads4Games;
+
 use Tagcade\Bundle\AppBundle\Command\GetDataCommand as BaseGetDataCommand;
 use Tagcade\DataSource\PartnerFetcherInterface;
-
+use Symfony\Component\Console\Input\InputInterface;
+use Tagcade\DataSource\PartnerParamInterface;
 
 class GetDataCommand extends BaseGetDataCommand  {
 
@@ -17,6 +18,25 @@ class GetDataCommand extends BaseGetDataCommand  {
         ;
 
         parent::configure();
+    }
+
+    protected function getDataForPublisher(InputInterface $input, $publisherId, PartnerParamInterface $params, array $config, $dataPath)
+    {
+
+        $startDate = $params->getStartDate();
+        $endDate = $params->getEndDate();
+        $end = $endDate->modify('+1 day');
+        $interval = new \DateInterval('P1D');
+        $dateRange = new \DatePeriod($startDate, $interval ,$end);
+
+        foreach ($dateRange as $date) {
+            $params->setStartDate($date);
+            $params->setEndDate($date);
+
+            parent::getDataForPublisher($input, $publisherId, $params, $config, $dataPath);
+        }
+
+        return 0;
     }
 
     /**
