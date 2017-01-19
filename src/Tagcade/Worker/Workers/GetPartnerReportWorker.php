d9f937448fe1c6c6f47d8bd4605f53a317e24da2
Giang Coffee <giang.fet.hut@gmail.com>
2017-01-19 10:33:33 +0700

    add workers to accept jobs

---
+++ src/Tagcade/Worker/Workers/GetPartnerReportWorker.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tagcade\Worker\Workers;
+
+
+// responsible for doing the background tasks assigned by the manager
+// all public methods on the class represent tasks that can be done
+
+use stdClass;
+
+class GetPartnerReportWorker
+{
+    public function getPartnerReport(StdClass $params)
+    {
+    }
+}
