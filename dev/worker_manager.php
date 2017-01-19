292dc9da3a03b4b6b4f4d8b50d5a58096fa838c5
Giang Coffee <giang.fet.hut@gmail.com>
2017-01-19 14:34:35 +0700

    add script to put pseudo jbo

---
+++ dev/worker_manager.php
@@ -0,0 +1,30 @@
+<?php
+namespace tagcade\dev;
+use AppKernel;
+use stdClass;
+
+$loader = require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->boot();
+
+$container = $kernel->getContainer();
+$queue = $container->get('leezy.pheanstalk');
+const TUBE = 'fetcher-worker';
+
+
+$params = new StdClass();
+$params->publisherId = 1;
+$params->type = 'ui';
+$params->cname = 'sovrn';
+$params->param = '{"username": "admin", "password": "123456"}';
+$payload = new StdClass;
+
+$payload->task = 'getPartnerReport';
+$payload->params = $params;
+
+$queue
+    ->useTube(TUBE)
+    ->put(json_encode($payload))
+;
