e009a9d0396514ae3c1be98e9240a386e789e151
lindan999 <linhvv.tt@gmail.com>
2017-04-21 08:46:12 +0700

    lkqd improvement: support timezone and dimensions

---
+++ src/Tagcade/Service/Fetcher/Params/Lkqd/LkqdPartnerParamInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tagcade\Service\Fetcher\Params\Lkqd;
+
+interface LkqdPartnerParamInterface
+{
+    /**
+     * @return String
+     */
+    public function getTimeZone();
+
+    /**
+     * @return mixed
+     */
+    public function getDimensions();
+}
---
+++ src/Tagcade/Service/Fetcher/Params/Lkqd/LkqdPartnerParams.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tagcade\Service\Fetcher\Params\Lkqd;
+
+
+use Tagcade\Service\Fetcher\Params\PartnerParams;
+use Tagcade\Service\Integration\ConfigInterface;
+
+class LkqdPartnerParams extends PartnerParams implements LkqdPartnerParamInterface
+{
+    const PARAM_KEY_TIMEZONE = 'timezone';
+    const PARAM_KEY_DIMENSIONS = 'dimensions';
+    const DEFAULT_TIME_ZONE = 'UTC';
+
+    /**
+     * @var string
+     */
+    protected $timezone;
+
+    protected $dimensions;
+
+    /**
+     * CedatoPartnerParams constructor.
+     * @param ConfigInterface $config
+     */
+    public function __construct(ConfigInterface $config)
+    {
+        parent::__construct($config);
+        $this->timezone = $config->getParamValue(self::PARAM_KEY_TIMEZONE, self::DEFAULT_TIME_ZONE);
+        $this->dimensions = $config->getParamValue(self::PARAM_KEY_DIMENSIONS, []);
+    }
+
+    /**
+     * @return String
+     */
+    public function getTimeZone()
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDimensions()
+    {
+        return $this->dimensions;
+    }
+}
