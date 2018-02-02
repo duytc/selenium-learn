36f7254b74e395c03842279ee6cf8e6f5cbc220a
dunght160387 <dunght1603@gmail.com>
2018-02-02 09:47:57 +0700

    Fix diff month between two date for Streamrail fetchers

--- src/Tagcade/Service/Fetcher/Fetchers/StreamRailExternal/Widget/DateSelectWidget.php
+++ src/Tagcade/Service/Fetcher/Fetchers/StreamRailExternal/Widget/DateSelectWidget.php
@@ -5,13 +5,12 @@ namespace Tagcade\Service\Fetcher\Fetchers\StreamRailExternal\Widget;
 use DateTime;
 use Exception;
 use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver\WebDriverSelect;
-use Symfony\Component\Validator\Constraints\Date;
 use Tagcade\Service\Fetcher\Fetchers\PulsePoint\Widget\AbstractWidget;
 
 class DateSelectWidget extends AbstractWidget
 {
     protected $firstDate = false;
+
     /**
      * @param DateTime $startDate
      * @param DateTime $endDate
@@ -71,14 +70,11 @@ class DateSelectWidget extends AbstractWidget
     private function selectDateWithCustomId($startDate, $id = null)
     {
         if ($this->firstDate instanceof DateTime) {
-            // $monthsBack = $this->firstDate->diff($startDate)->format('%m'); // wrong diff, TODO: remove
-            $monthsBack = $this->firstDate->format('n') - $startDate->format('n');
-        }
-        else {
+            $monthsBack = $this->diffMonths($startDate, $this->firstDate);
+        } else {
             $this->firstDate = $startDate;
             $now = new DateTime('now');
-            // $monthsBack = $this->firstDate->diff($startDate)->format('%m'); // wrong diff, TODO: remove
-            $monthsBack = $now->format('n') - $startDate->format('n');
+            $monthsBack = $this->diffMonths($startDate, $now);
         }
 
         $clickMonthBack = $this->driver->findElement(WebDriverBy::cssSelector('button.ember-power-calendar-nav-control.ember-power-calendar-nav-control--previous'));
@@ -96,5 +92,28 @@ class DateSelectWidget extends AbstractWidget
                 break;
             }
         }
+    }
+
+    /**
+     * calculate diff months from startDate to endDate
+     *
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     * @return int positive value if endDate greater than startDate and vice verse
+     */
+    private function diffMonths(DateTime $startDate, DateTime $endDate)
+    {
+        /*
+         * DO NOT USE $diffMonths = $this->firstDate->diff($startDate)->format('%m');
+         * e.g:
+         *   2017-11-28->diff(2017-12-30): diff-months=1, expected=1 OK because of diffDay=32 >= 30
+         *   2017-11-28->diff(2017-12-26): diff-months=0, expected=1 FAILED because of diffDay=28 < 30
+         *   2017-12-10->diff(2018-01-10): diff-months=1, expected=1 OK because of diffDay=30 >= 30
+         *   2017-12-30->diff(2018-01-02): diff-months=1, expected=1 FAILED because of diffDay=28 < 30
+         */
+        $monthStartDate = (int)($startDate->format('Y')) * 12 + (int)($startDate->format('m'));
+        $monthEndDate = (int)($endDate->format('Y')) * 12 + (int)($endDate->format('m'));
+
+        return ($monthEndDate - $monthStartDate);
     }
 }
